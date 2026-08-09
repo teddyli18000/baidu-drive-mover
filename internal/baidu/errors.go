191976daeb7072c28d596299667f8a98dd83ab14
Xinchen Lee <teddyli18000@gmail.com>
2026-08-09 15:59:12 +0800

    baidu: model staging and quota errors

--- internal/baidu/errors.go
+++ internal/baidu/errors.go
@@ -11,6 +11,8 @@ var (
 	ErrWrongPassword        = errors.New("wrong share extraction code")
 	ErrShareExpired         = errors.New("Baidu share is unavailable or expired")
 	ErrVerificationRequired = errors.New("Baidu security verification required")
+	ErrQuotaExceeded        = errors.New("Baidu storage quota exceeded")
+	ErrTransferConflict     = errors.New("Baidu staging transfer conflict")
 )
 
 type RemoteError struct {
@@ -20,4 +22,30 @@ type RemoteError struct {
 
 func (e *RemoteError) Error() string {
 	return fmt.Sprintf("Baidu %s failed with errno %d", e.Operation, e.Errno)
+}
+
+type PCSRemoteError struct {
+	Operation string
+	Code      int
+}
+
+func (e *PCSRemoteError) Error() string {
+	return fmt.Sprintf("Baidu PCS %s failed with error_code %d", e.Operation, e.Code)
+}
+
+type TransferLimitError struct {
+	Target int
+	Limit  int
+}
+
+func (e *TransferLimitError) Error() string {
+	return fmt.Sprintf("Baidu transfer contains %d files but account limit is %d", e.Target, e.Limit)
+}
+
+type StagingConflictError struct {
+	Name string
+}
+
+func (e *StagingConflictError) Error() string {
+	return fmt.Sprintf("Baidu staging contains conflicting object %q", e.Name)
 }
